b826307d12a6733ef357674ebd69d66c2d95cb49
Subhendu Singh <ssubhendu988@gmail.com>
2026-08-10 09:00:39 +0530

    feat: add login, register routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,20 @@
 import Fastify from "fastify";
 import { connectDb } from "./lib/prisma";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import fastifyJwt from "@fastify/jwt";
+import { authRoutes } from "./routes/auth.routes";
 
 const PORT = 3000
 
 const fastify = Fastify({
   logger: true
+}).withTypeProvider<ZodTypeProvider>();
+
+fastify.setValidatorCompiler(validatorCompiler)
+fastify.setSerializerCompiler(serializerCompiler)
+
+fastify.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET || "jwt-secret-key"
 })
 
 fastify.get('/', async (request, reply) => {
@@ -12,6 +22,10 @@ fastify.get('/', async (request, reply) => {
     success: true,
     message: 'Order and settlements server is running'
   }
+})
+
+fastify.register(authRoutes, {
+  prefix: "/api/auth"
 })
 
 await connectDb(fastify.log);
---
+++ server/src/routes/auth.routes.ts
@@ -0,0 +1,62 @@
+import type { FastifyInstance } from "fastify";
+import { type ZodTypeProvider } from "fastify-type-provider-zod";
+import { loginSchema, registerSchema } from "../validator/auth.validator";
+import { prisma } from "../lib/prisma";
+import { password } from "bun";
+import { HTTP_STATUS } from "../constants/http-status";
+
+export async function authRoutes(app: FastifyInstance) {
+  const fastify = app.withTypeProvider<ZodTypeProvider>();
+
+  fastify.post("/register", {
+    schema: {
+        body: registerSchema,
+    },
+  }, async(request, reply) => {
+    const {email, password} = request.body;
+
+    const existingUser = await prisma.user.findUnique({where: {email}});
+    if (existingUser) {
+      return reply.status(HTTP_STATUS.BAD_REQUEST).send({message: "User already exists with this email"})
+    } 
+
+    const hashedPassword = await Bun.password.hash(password);
+
+    const user = await prisma.user.create({
+        data: {email, password: hashedPassword},
+        select: {id: true, email: true, createdAt: true},
+    })
+
+    const token = app.jwt.sign({
+        id: user.id, email: user.email
+    })
+
+    return reply.status(HTTP_STATUS.CREATED).send({message: "User registered successfully", token, user});
+  });
+
+  fastify.post("/login", {
+    schema: { 
+        body: loginSchema
+    }
+  }, async (request, reply) => {
+      const {email, password} = request.body;
+
+      const user = await prisma.user.findUnique({ where: {email}});
+      if (!user) {
+          return reply.status(HTTP_STATUS.UNAUTHORIZED).send({ error: "Invalid email or password"});
+      }
+
+      const isPasswordvalid = await Bun.password.verify(password, user.password)
+      if(!isPasswordvalid) {
+          return reply.status(HTTP_STATUS.UNAUTHORIZED).send({ error: "Invalid email or password"});
+      }
+
+      const token = app.jwt.sign({ id: user.id, email: user.email });
+
+      return reply.status(HTTP_STATUS.OK).send({
+          message: "Login successful",
+          user: {id: user.id, email: user.email},
+          token
+      })
+  })
+}
